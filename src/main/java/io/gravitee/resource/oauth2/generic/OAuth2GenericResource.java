f4e8d49b0b9f328096f3cb194f6828af005d30b0
brasseld <brasseld@gmail.com>
2017-04-10 15:53:13 +0200

    feat(oauth2): Refactor OAuth2 generic resource configuration and use the resource-oauth2-provider-api
    Closes gravitee-io/issues#534

---
+++ src/main/java/io/gravitee/resource/oauth2/generic/OAuth2GenericResource.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.resource.oauth2.generic;
+
+import io.gravitee.common.http.HttpHeaders;
+import io.gravitee.common.http.HttpStatusCode;
+import io.gravitee.common.http.MediaType;
+import io.gravitee.gateway.api.handler.Handler;
+import io.gravitee.resource.oauth2.api.OAuth2Resource;
+import io.gravitee.resource.oauth2.api.OAuth2Response;
+import io.gravitee.resource.oauth2.generic.configuration.OAuth2ResourceConfiguration;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.util.Base64;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author Titouan COMPIEGNE (titouan.compiegne at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class OAuth2GenericResource extends OAuth2Resource<OAuth2ResourceConfiguration> {
+
+    private final Logger logger = LoggerFactory.getLogger(OAuth2GenericResource.class);
+
+    private static final String HTTPS_SCHEME = "https";
+
+    private static final char AUTHORIZATION_HEADER_SCHEME_SEPARATOR = ' ';
+    private static final char AUTHORIZATION_HEADER_VALUE_BASE64_SEPARATOR = ':';
+
+    private HttpClient httpClient;
+
+    @Override
+    protected void doStart() throws Exception {
+        super.doStart();
+
+        logger.info("Starting an OAuth2 resource using authorization server at {}", configuration().getIntrospectionEndpoint());
+        HttpClientOptions httpClientOptions = new HttpClientOptions();
+        httpClientOptions.setVerifyHost(false).setTrustAll(true);
+
+        httpClient = Vertx.vertx().createHttpClient(httpClientOptions);
+    }
+
+    @Override
+    protected void doStop() throws Exception {
+        super.doStop();
+
+        if (httpClient != null) {
+            httpClient.close();
+        }
+    }
+
+    @Override
+    public void introspect(String accessToken, Handler<OAuth2Response> responseHandler) {
+        OAuth2ResourceConfiguration configuration = configuration();
+        URI introspectionUri = URI.create(configuration.getIntrospectionEndpoint());
+
+        final int port = introspectionUri.getPort() != -1 ? introspectionUri.getPort() :
+                (HTTPS_SCHEME.equals(introspectionUri.getScheme()) ? 443 : 80);
+
+        StringBuilder introspectionUriBuilder = new StringBuilder(configuration.getIntrospectionEndpoint());
+
+        if (configuration.isTokenIsSuppliedByQueryParam()) {
+            introspectionUriBuilder
+                    .append('?').append(configuration.getTokenQueryParamName())
+                    .append('=').append(accessToken);
+        }
+
+        String introspectionEndpointURI = introspectionUriBuilder.toString();
+        logger.debug("Introspect access token by requesting {} [{}]", introspectionEndpointURI,
+                configuration.getIntrospectionEndpointMethod());
+
+        HttpClientRequest request = httpClient.request(
+                HttpMethod.valueOf(configuration.getIntrospectionEndpointMethod().toUpperCase()),
+                port,
+                introspectionUri.getHost(),
+                introspectionEndpointURI);
+
+        if (configuration().isUseClientAuthorizationHeader()) {
+            String authorizationHeader = configuration.getClientAuthorizationHeaderName();
+            String authorizationValue = configuration.getClientAuthorizationHeaderScheme().trim() +
+                    AUTHORIZATION_HEADER_SCHEME_SEPARATOR +
+                    Base64.getEncoder().encodeToString(
+                            (configuration.getClientId() +
+                                    AUTHORIZATION_HEADER_VALUE_BASE64_SEPARATOR +
+                                    configuration.getClientSecret()).getBytes());
+            request.headers().add(authorizationHeader, authorizationValue);
+            logger.debug("Set client authorization using HTTP header {} with value {}", authorizationHeader, authorizationValue);
+        }
+
+        // Set `Accept` header to ask for application/json content
+        request.headers().add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON);
+
+        if (configuration.isTokenIsSuppliedByHttpHeader()) {
+            request.headers().add(configuration.getTokenHeaderName(), accessToken);
+        }
+
+        request.handler(response -> response.bodyHandler(buffer -> {
+            logger.debug("Introspection endpoint returns a response with a {} status code", response.statusCode());
+            if (response.statusCode() == HttpStatusCode.OK_200) {
+                responseHandler.handle(new OAuth2Response(true, buffer.toString()));
+            } else {
+                responseHandler.handle(new OAuth2Response(false, buffer.toString()));
+            }
+        }));
+
+        request.exceptionHandler(event -> {
+            logger.error("An error occurs while checking OAuth2 token", event);
+            responseHandler.handle(new OAuth2Response(false, event.getMessage()));
+        });
+
+        request.end();
+    }
+}
---
+++ src/main/java/io/gravitee/resource/oauth2/generic/configuration/OAuth2ResourceConfiguration.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.resource.oauth2.generic.configuration;
+
+import io.gravitee.resource.api.ResourceConfiguration;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author Titouan COMPIEGNE (titouan.compiegne at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class OAuth2ResourceConfiguration implements ResourceConfiguration {
+
+    private String introspectionEndpoint;
+
+    private String introspectionEndpointMethod;
+
+    private String clientId;
+
+    private String clientSecret;
+
+    private boolean useClientAuthorizationHeader;
+
+    private String clientAuthorizationHeaderName;
+
+    private String clientAuthorizationHeaderScheme;
+
+    private boolean tokenIsSuppliedByQueryParam;
+
+    private String tokenQueryParamName;
+
+    private boolean tokenIsSuppliedByHttpHeader;
+
+    private String tokenHeaderName;
+
+    public String getIntrospectionEndpoint() {
+        return introspectionEndpoint;
+    }
+
+    public void setIntrospectionEndpoint(String introspectionEndpoint) {
+        this.introspectionEndpoint = introspectionEndpoint;
+    }
+
+    public String getIntrospectionEndpointMethod() {
+        return introspectionEndpointMethod;
+    }
+
+    public void setIntrospectionEndpointMethod(String introspectionEndpointMethod) {
+        this.introspectionEndpointMethod = introspectionEndpointMethod;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public boolean isUseClientAuthorizationHeader() {
+        return useClientAuthorizationHeader;
+    }
+
+    public void setUseClientAuthorizationHeader(boolean useClientAuthorizationHeader) {
+        this.useClientAuthorizationHeader = useClientAuthorizationHeader;
+    }
+
+    public String getClientAuthorizationHeaderName() {
+        return clientAuthorizationHeaderName;
+    }
+
+    public void setClientAuthorizationHeaderName(String clientAuthorizationHeaderName) {
+        this.clientAuthorizationHeaderName = clientAuthorizationHeaderName;
+    }
+
+    public String getClientAuthorizationHeaderScheme() {
+        return clientAuthorizationHeaderScheme;
+    }
+
+    public void setClientAuthorizationHeaderScheme(String clientAuthorizationHeaderScheme) {
+        this.clientAuthorizationHeaderScheme = clientAuthorizationHeaderScheme;
+    }
+
+    public boolean isTokenIsSuppliedByQueryParam() {
+        return tokenIsSuppliedByQueryParam;
+    }
+
+    public void setTokenIsSuppliedByQueryParam(boolean tokenIsSuppliedByQueryParam) {
+        this.tokenIsSuppliedByQueryParam = tokenIsSuppliedByQueryParam;
+    }
+
+    public String getTokenQueryParamName() {
+        return tokenQueryParamName;
+    }
+
+    public void setTokenQueryParamName(String tokenQueryParamName) {
+        this.tokenQueryParamName = tokenQueryParamName;
+    }
+
+    public boolean isTokenIsSuppliedByHttpHeader() {
+        return tokenIsSuppliedByHttpHeader;
+    }
+
+    public void setTokenIsSuppliedByHttpHeader(boolean tokenIsSuppliedByHttpHeader) {
+        this.tokenIsSuppliedByHttpHeader = tokenIsSuppliedByHttpHeader;
+    }
+
+    public String getTokenHeaderName() {
+        return tokenHeaderName;
+    }
+
+    public void setTokenHeaderName(String tokenHeaderName) {
+        this.tokenHeaderName = tokenHeaderName;
+    }
+}
